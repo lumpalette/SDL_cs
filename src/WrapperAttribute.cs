ccccfa5de643c94679adede9972efd0b51c24011
lumPalette <lumPalette@gmail.com>
2024-05-31 23:05:17 -0600

    wth i forgot to commit the wrapperattribute LOL

---
+++ src/WrapperAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace SDL3;
+
+/// <summary>
+/// Indicates that this type is a wrapper for another type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Struct)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public class WrapperAttribute : Attribute;
