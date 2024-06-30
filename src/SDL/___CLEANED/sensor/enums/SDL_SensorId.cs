73df46b3332505bd7c3fa64361296a73c2cce817
lumPalette <lumPalette@gmail.com>
2024-06-30 02:47:40 -0600

    cleaned sensor

---
+++ src/SDL/___CLEANED/sensor/enums/SDL_SensorId.cs
@@ -0,0 +1,17 @@
+﻿namespace SDL_cs;
+
+/// <summary>
+/// This is a unique ID for a sensor for the time it is connected to the system, and is never reused for the
+/// lifetime of the application.
+/// </summary>
+/// <remarks>
+/// Refer to the official <see href="https://wiki.libsdl.org/SDL3/SDL_SensorID">documentation</see> for more details.
+/// </remarks>
+[Typedef]
+public enum SDL_SensorId : uint
+{
+	/// <summary>
+	/// An invalid sensor ID. Used when a function that returns an <see cref="SDL_SensorId"/> fails.
+	/// </summary>
+	Invalid = 0
+}
--- src/SDL/___CLEANED/sensor/structs/SDL_Sensor.cs
+++ src/SDL/___CLEANED/sensor/structs/SDL_Sensor.cs
@@ -3,8 +3,5 @@
 /// <summary>
 /// The structure used to identify a sensor in SDL.
 /// </summary>
-/// <remarks>
-/// This structure is an opaque type.
-/// </remarks>
 [Opaque]
 public readonly struct SDL_Sensor;
