dcdba31cdb4337a47d43fa2b7fdd865f97e259c5
lumPalette <lumPalette@gmail.com>
2024-07-25 23:50:10 -0600

    added SDL_Exception for convenience

---
+++ src/SDL/SDL_Exception.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SDL_cs;
+
+/// <summary>
+/// An exception that can be thrown whenever an SDL function fails.
+/// </summary>
+[Serializable]
+public class SDL_Exception : Exception
+{
+	/// <summary>
+	/// Instantiates a new <see cref="SDL_Exception"/> that shows the error message returned by <see cref="SDL.GetError"/>.
+	/// </summary>
+	public SDL_Exception() : base($"SDL error: '{SDL.GetError()}'.") { }
+
+	/// <summary>
+	/// Instantiates a new <see cref="SDL_Exception"/> with the provided error message. The error message returned by
+	/// <see cref="SDL.GetError"/> is also included.
+	/// </summary>
+	/// <param name="message">The message that describes the error.</param>
+	public SDL_Exception(string message) : base($"{message} SDL error: '{SDL.GetError()}'.") { }
+
+	/// <summary>
+	/// Instantiates a new <see cref="SDL_Exception"/> with the provided error message. The error message returned by
+	/// <see cref="SDL.GetError"/> is also included.
+	/// </summary>
+	/// <param name="message">The message that describes the error.</param>
+	public SDL_Exception(string message, Exception inner) : base($"{message} SDL error: '{SDL.GetError()}'.", inner) { }
+}
