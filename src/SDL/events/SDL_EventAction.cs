d0c0b60cf77181e36714242fdfc6fd05ebe89ce0
lumPalette <lumPalette@gmail.com>
2024-09-09 18:51:17 -0600

    document SDL_EventAction properly
    finally

--- src/SDL/events/SDL_EventAction.cs
+++ src/SDL/events/SDL_EventAction.cs
@@ -1,7 +1,7 @@
 ﻿namespace SDL3;
 
 /// <summary>
-/// The actions that can be done in <see cref="SDL.PeepEvents(SDL_Event*, int, SDL_EventAction, SDL_EventType, SDL_EventType)"/>.
+/// The type of action to request from <see cref="SDL.PeepEvents(SDL_Event*, int, SDL_EventAction, SDL_EventType, SDL_EventType)"/>.
 /// </summary>
 /// <remarks>
 /// Refer to the official <see href="https://wiki.libsdl.org/SDL3/SDL_EventAction">documentation</see> for more details.
@@ -9,19 +9,17 @@
 public enum SDL_EventAction
 {
 	/// <summary>
-	/// Events will be added to the back of the event queue.
+	/// Add events to the back of the queue.
 	/// </summary>
 	AddEvent,
 
 	/// <summary>
-	/// Events at the front of the event queue, within the specified minimum and maximum type, will be returned
-	/// to the caller and will not be removed from the queue.
+	/// Check but don't remove events from the queue front.
 	/// </summary>
 	PeekEvent,
 
 	/// <summary>
-	/// Events at the front of the event queue, within the specified minimum and maximum type, will be returned
-	/// to the caller and will be removed from the queue.
+	/// Retrieve/remove events from the front of the queue.
 	/// </summary>
 	GetEvent
 }
